a238c8594da98d757c82453b0048a32344a5c3d4
Tom Breza <tom@pcservicegroup.co.uk>
2017-04-19 16:45:38 +0100

    another version of the Programming Principles and Practise Using C++

--- TemperatureTest/TemperatureTest.cpp
+++ TemperatureTest/TemperatureTest.cpp
@@ -8,25 +8,24 @@
 
 int main()
 {
-	vector<double>temps; // temperature
-
-	for (double temp; cin >> temp;)
-		temps.push_back(temp);
-
 	double sum = 0;
-	double high_temp = 0;
-	double low_temp = 0;
+	double high_temp = -1000; // initializate to imposibly low
+	double low_temp = 1000; // initializate to imposibly high
+	int no_of_temps = 0;
 
-	for (int x : temps)
+	//vector<double>temps; // temperature
+
+	for (double temp; cin >> temp;) //read temp
 	{
-		if (x > high_temp) high_temp = x; // find high temp
-		if (x < low_temp) low_temp = x; // find low temp
-		sum += x; // compute temperatures
+		++no_of_temps; //count temperatures
+		sum += temp; // compute sum
+		if (temp > high_temp)high_temp = temp; //find high
+		if (temp < low_temp) low_temp = temp; // find low
 	}
 
 	cout << "High temperature: " << high_temp << endl;
 	cout << "Low temeperature: " << low_temp << endl;
-	cout << "Average temperature: " << sum / temps.size() << endl;
+	cout << "Average temperature: " << sum / no_of_temps << endl;
 
 
 	system("pause");
